0b6e7bb5b4303c6708379b29d3f70d96a9d55b54
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-06-03 13:31:47 +0100

    getTso debug

--- src/airtable/delete.js
+++ src/airtable/delete.js
@@ -1,15 +1,21 @@
 
 
 const deleteRecord = async (base, table, recordID) => {
+    console.log(base);
+    console.log(table);
+    console.log(recordID);
     try {
-        await base(table).destroy([recordID], function (err, deletedRecords) {
-            // if (err) {
-            //     console.error(err);
-            //     return;
-            // }
+        await base(table).destroy(recordID, function (err, deletedRecords) {
+            if (err) {
+                console.error(err);
+                return;
+            }
             console.log('Deleted', deletedRecords.length, 'records');
         });
-    }catch(err){console.log(err)}
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 export default deleteRecord;
